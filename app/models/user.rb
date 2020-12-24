ad3ad9a4631f051a0dc30c0db212b5114e10ca37
yusa0730 <isizawa5226@gmail.com>
2020-12-24 17:27:36 +0900

    ログイン機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :name, presence:true
+  enum gender: {男性:0, 女性:1}
 end
